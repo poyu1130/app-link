3c8c844175b4ce36406458549bb1b5b4c5d645eb
陳柏佑 <30464739+poyu1130@users.noreply.github.com>
2025-02-03 21:25:37 +0800

    Update browser-deeplink.js

--- testlink/deepLinkTest/browser-deeplink.js
+++ testlink/deepLinkTest/browser-deeplink.js
@@ -12,10 +12,10 @@
       console.log("開啟 app!")
       
 if (window.hidden) {
-    console.log('頁面目前不可見');
+    alert('頁面目前不可見');
     // 進行相應的處理
     // 將 timer 清除
-    clearTimeout(timeout)
+    clearTimeout(timeout);
   } else {
     console.log('頁面目前可見');
     // 回復或其他動作
